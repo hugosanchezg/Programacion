47424d34432c4f2ec22bee5245c28f50c26b3b74
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2021-10-27 12:50:42 +0200

    Add files via upload

---
+++ t5/Ej7.java
@@ -0,0 +1,30 @@
+//Si contraseña introducida es igual a contraseña (real):
+// -“La caja fuerte se ha abierto satisfactoriamente”
+
+//Si contraseña introducida no es igual a contraseña (real):
+// -“Lo siento, esa no es la combinación”
+
+public class Ej7{
+public static void main(String[]args){
+  
+  int i=0;
+  int nintentos=4;
+  
+  System.out.println("Control de acceso a la caja fuerte");
+
+for(i=0; i<=1234; i++){
+  System.out.println("Introduzca la contraseña (de 4 dígitos): ");
+    int contraseñaintroducida=Integer.parseInt(System.console().readLine());
+    i=contraseñaintroducida;
+    
+    if(i==1234){
+      System.out.println(+contraseñaintroducida+": La caja fuerte se ha abierto satisfactoriamente");
+    }else{
+      System.out.println(+contraseñaintroducida+": Lo siento, esa no es la combinación");
+      --nintentos;
+      }if(nintentos<1){
+      System.out.println("Se han acabado tus intentos");
+      //Comando para acabar el programa en el caso de que se acaben los intentos:
+      System.exit(0);
+      }
+}}}
