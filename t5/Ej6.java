2a074ec819ef4955fbd4f9b9b3cf73b9d980c4e9
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2021-10-27 10:36:11 +0200

    Add files via upload

---
+++ t5/Ej6.java
@@ -0,0 +1,12 @@
+public class Ej6{
+public static void main(String[]args){
+
+  int i=320;
+  System.out.println("números del 320 al 160, contando de 20 en 20 hacia atrás");
+
+do{
+  System.out.println(i);
+  i-=20;
+  }while(i>=160);
+
+}}
