cf1b09d07c167cf9d03adaac334b47ea47cf0bc8
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2021-10-27 10:25:25 +0200

    Add files via upload

---
+++ t5/Ej2.java
@@ -0,0 +1,11 @@
+public class Ej2{
+public static void main(String[]args){
+
+  int i=0;
+  System.out.println("Múltiplos de 5 del 0 al 100");
+
+while(i<=100){
+    i+=5;
+    System.out.println(i);
+}
+}}
