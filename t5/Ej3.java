673324a4206ddea6e9245d826de82b9c384611be
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2021-10-27 10:29:43 +0200

    Add files via upload

---
+++ t5/Ej3.java
@@ -0,0 +1,12 @@
+public class Ej3{
+public static void main(String[]args){
+    
+  int i=0;
+  System.out.println("Múltiplos de 5 del 0 al 100");
+    
+do{
+  System.out.println(i);
+  i+=5;
+  }while(i<=100);
+
+}}
