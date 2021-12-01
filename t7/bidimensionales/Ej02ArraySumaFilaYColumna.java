990cae936784b61fb8ea7d247fc04cd76b628849
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2021-12-01 10:22:46 +0100

    Add files via upload

---
+++ t7/bidimensionales/Ej02ArraySumaFilaYColumna.java
@@ -0,0 +1,45 @@
+public class Ej02ArraySumaFilaYColumna{
+public static void main(String[]args){
+  
+  
+  int[][] numero = new int[4][5];
+  int fila;
+  int columna;
+  int sumafila=0;
+  int sumacolumna=0;
+  int sumatotal=0;
+  
+  System.out.println("Introduce 20 números: ");
+  
+  //Pedimos los números
+  for(fila = 0; fila < 4; fila++){
+    for(columna = 0; columna < 5; columna++){
+      System.out.print("Fila "+fila+", columna "+columna+": ");
+      numero[fila][columna]=Integer.parseInt(System.console().readLine());
+    }
+  }
+  
+  //Sumamos las filas
+  for(fila = 0; fila < 4; fila++){
+    for(columna = 0; columna < 5; columna++){
+      System.out.printf("%4d   ",numero[fila][columna]);
+      sumafila+=numero[fila][columna];
+    }
+    System.out.printf("|%4d\n",sumafila);
+  }
+  
+  //Sumamos las columnas
+  for(columna = 0; columna < 5; columna++){
+    System.out.print("_______");
+  }
+  
+  System.out.println("|______");
+  for(columna = 0; columna < 5; columna++){
+    for(fila = 0; fila < 4; fila++){
+      sumacolumna+=numero[fila][columna];
+    }
+    sumatotal+=sumacolumna;
+    System.out.printf("%4d   ",sumacolumna);
+  }
+  System.out.printf("|%4d   ",sumatotal);
+}}
