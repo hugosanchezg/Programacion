36c1027c06a9fddd74e2f86256188d0295460612
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2021-12-01 10:49:33 +0100

    Add files via upload

---
+++ t7/bidimensionales/Ej04SumaRETARDO.java
@@ -0,0 +1,48 @@
+public class Ej04SumaRETARDO{
+public static void main(String[]args)
+throws InterruptedException{
+  
+  
+  int[][] numero = new int[4][5];
+  int fila;
+  int columna;
+  int sumafila=0;
+  int sumacolumna=0;
+  int sumatotal=0;
+  
+  System.out.println("Introduce 20 números: ");
+  
+  //Pedimos los números
+  for(fila = 0; fila < 4; fila++){
+    for(columna = 0; columna < 5; columna++){
+      numero[fila][columna]=(int)(Math.random()*1000)+100;
+    }
+  }
+  
+  //Sumamos las filas
+  for(fila = 0; fila < 4; fila++){
+    for(columna = 0; columna < 5; columna++){
+      System.out.printf("%4d   ",numero[fila][columna]);
+      sumafila+=numero[fila][columna];
+    }
+    System.out.printf("|%4d\n",sumafila);
+    Thread.sleep(600);
+  }
+  
+  //Sumamos las columnas
+  for(columna = 0; columna < 5; columna++){
+    System.out.print("_______");
+  }
+  
+  System.out.println("|______");
+  for(columna = 0; columna < 5; columna++){
+    for(fila = 0; fila < 4; fila++){
+      sumacolumna+=numero[fila][columna];
+    }
+    sumatotal+=sumacolumna;
+    System.out.printf("%4d   ",sumacolumna);
+    Thread.sleep(600);
+  }
+  System.out.printf("%4d",sumatotal);
+  
+}}
