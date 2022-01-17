430fce8accac2b9164191f01e8a676fd9bb12be3
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2022-01-17 18:17:20 +0100

    Add files via upload

---
+++ t8/Ej39convierteEnPalabras.java
@@ -0,0 +1,41 @@
+package Ejercicios;
+import java.util.Scanner;
+
+public class Ej39convierteEnPalabras {
+	
+	public static void main(String[] args) {
+		
+		int[] numeros = {123, 234, 34543};
+		
+		for (int numero : numeros) {
+			
+			System.out.print("\nEl número " + numero +  " es: " + convierteEnPalabras(numero));
+			
+		}
+		
+		
+		
+	}
+		
+	public static String convierteEnPalabras(int n) {
+		
+		String[] palabras = {"cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
+		String resultado = "";
+		
+		int i;
+		//123
+		for(i = 1; i < Ej1al14.digitos(n); i++) {
+			
+			resultado = resultado + palabras[Ej1al14.digitoN(n, i)] + ", ";
+			
+		}
+		
+		//Sumamos el último dígito de como se ha quedado la i porque no va con comas
+		resultado += palabras[Ej1al14.digitoN(n, i)];
+		
+		return resultado;
+		
+	}
+	
+	
+}
