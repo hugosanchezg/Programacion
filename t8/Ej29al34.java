48ea79d8bb03b5880e3d749a4b2b22ee02aebf56
Hugo <91953221+hugosanchezg@users.noreply.github.com>
2022-01-24 12:20:03 +0100

    Add files via upload

--- t8/Ej29al34.java
+++ t8/Ej29al34.java
@@ -1,4 +1,4 @@
-package Ejercicios;
+package funciones;
 
 import java.util.Scanner;
 
@@ -107,9 +107,9 @@ public class Ej29al34 {
 	
 	public static void muestraArrayBiInt(int[][] array) {
 		
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0; i < array.length; i++) {
 			
-			for(int j = 0; j < 5; j++) {
+			for(int j = 0; j < array[0].length; j++) {
 				
 				System.out.print(array[i][j] + " ");
 				
